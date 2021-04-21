368bc4468d1a849b74e2bd8a5bb1f5df59a63268
Massou <massou.lionel@gmail.com>
2021-04-21 11:25:22 +0200

    First version "done"

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,33 +2,20 @@ import React from "react";
 import "./Card.css"
 
 class Card extends React.Component {
-
-    // constructor(props) {
-    //     super(props);
-    //     this.state = {
-    //         reveal: false,
-    //     }
-
-    //     this.flip = this.flip.bind(this)
-    // }
-       
-
     render() {
-        let content; 
-
-        if (this.props.reveal){
-            content = this.props.content;
-            console.log(content)
-        } else {
-            content = "Blank"
-        }  
-
-        return (
-            <div onClick = {this.props.flip} className={`Card ${this.props.reveal ? 'reveal': 'blank'}`} >
-                {content}
-            </div>
-        );
+      let content;
+      if(this.props.reveal) {
+        content = this.props.content;
+      } else {
+        content = '?'
+      }
+      return (
+        <div onClick={this.props.flip} className={`Card ${this.props.reveal ? 'reveal': ''}`}>
+        {/* <div onClick={this.props.flip} className={`Card ${this.props.reveal ? 'reveal': 'test'}`}> */}
+          {content}
+        </div>
+      )
     }
-}
-
-export default Card; 
+  }
+  
+  export default Card;
